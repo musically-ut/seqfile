1e8f88305b0485fc9a54d86dd20691e66105471c
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-04-18 08:28:46 +0200

    Add a basic setup.py for easy installation through pip.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name             = "seqfile",
+    version          = "0.0.1",
+    author           = "Utkarsh Upadhyay",
+    author_email     = "musically.ut@gmail.com",
+    description      = ("Find the next file in a sequence of files in a thread-safe way."),
+    license          = "MIT",
+    keywords         = "file threadsafe sequence",
+    url              = "https://github.com/musically-ut/seqfile",
+    packages         = ['seqfile'],
+    long_description = read('README.md'),
+    classifiers      = [
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+        "Natural Language :: English",
+    ],
+)
