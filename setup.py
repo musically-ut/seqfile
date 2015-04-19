4675118d9ab398be32fc4a732a1e0114a23a2649
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-04-19 14:32:37 +0200

    Fix categorization in setup.py.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
         "Natural Language :: English",
     ],
 )
