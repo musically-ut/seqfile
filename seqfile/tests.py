fca0fb88734985565eafe3f3c95c7f802edb7513
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-04-19 19:26:00 +0200

    Simplify tests and test multiple file creations.

--- seqfile/tests.py
+++ seqfile/tests.py
@@ -15,82 +15,78 @@ def tempDir():
         _shutil.rmtree(d)
 
 
+prefix, suffix = 'mdl.', '.cPickle'
+fnameGen = lambda x : prefix + str(x) + suffix
+
 ###########################################################################################
 ## Tests
 ###########################################################################################
 
 def test_findNextFile_0_base():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
         assert _S.findNextFile(folder=d, prefix=prefix, suffix=suffix, base=0) == join(d, prefix + str(0) + suffix)
 
 def test_findNextFile_0_base_fnamegen():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
-        assert _S.findNextFile(folder=d, fnameGen=fnameGen, base=0) == join(d, prefix + str(0) + suffix)
+        assert _S.findNextFile(folder=d, fnameGen=fnameGen, base=0) == join(d, fnameGen(0))
 
 
 def test_findNextFile_1_base():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        assert _S.findNextFile(d, prefix, suffix, base=1) == join(d, prefix + str(1) + suffix)
+        assert _S.findNextFile(d, prefix, suffix, base=1) == join(d, fnameGen(1))
 
 def test_findNextFile_1_base_fnamegen():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
-        assert _S.findNextFile(folder=d, fnameGen=fnameGen, base=1) == join(d, prefix + str(1) + suffix)
+        assert _S.findNextFile(folder=d, fnameGen=fnameGen, base=1) == join(d, fnameGen(1))
 
 
 def test_findNextFile_with_file():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
-        assert _S.findNextFile(d, prefix, suffix) == join(d, prefix + str(1) + suffix)
-
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(1))
 
 def test_findNextFile_with_file_fnamegen():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
-        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, prefix + str(1) + suffix)
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(1))
 
 def test_findNextFile_with_files():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
         _S._doAtomicFileCreation( join(d, fnameGen(1)) )
-        assert _S.findNextFile(d, prefix, suffix) == join(d, prefix + str(2) + suffix)
-
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(2))
 
 def test_findNextFile_with_files_fnamegen():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
         _S._doAtomicFileCreation( join(d, fnameGen(1)) )
-        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, prefix + str(2) + suffix)
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(2))
+
+def test_findNextFile_many_files():
+    with tempDir() as d:
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(0))
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(1))
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(2))
+
+def test_findNextFile_many_files_fnamegen():
+    with tempDir() as d:
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(0))
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(1))
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(2))
 
 # These tests show the difference between prefix/suffix and fnameGen method.
 
 def test_findNextFile_with_files_non_consecutive():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
         _S._doAtomicFileCreation( join(d, fnameGen(2)) )
-        assert _S.findNextFile(d, prefix, suffix) == join(d, prefix + str(3) + suffix)
+        assert _S.findNextFile(d, prefix, suffix) == join(d, fnameGen(3))
 
 def test_findNextFile_with_files_non_consecutive_fnamegen():
     with tempDir() as d:
-        prefix, suffix = 'mdl.', '.cPickle'
-        fnameGen = lambda x : prefix + str(x) + suffix
         _S._doAtomicFileCreation( join(d, fnameGen(0)) )
         _S._doAtomicFileCreation( join(d, fnameGen(2)) )
-        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, prefix + str(1) + suffix)
+        assert _S.findNextFile(d, fnameGen=fnameGen) == join(d, fnameGen(1))
+
 
 
