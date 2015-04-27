04e80762cc7536e6dff7b2f696ccab0f294a9a2c
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-04-27 10:27:10 +0200

    Fix PEP8 violations.

--- seqfile/seqfile.py
+++ seqfile/seqfile.py
@@ -1,15 +1,18 @@
-import os      as _os
-import re      as _re
-import glob    as _glob
-import errno   as _errno
+import os as _os
+import re as _re
+import glob as _glob
+import errno as _errno
+
 import natsort as _natsort
 
-def _getStrBetween(prefix, s, suffix):
+
+def _strBetween(prefix, s, suffix):
     """Return the value inside s between prefix and suffix."""
     preIdx = s.index(prefix)
     sufIdx = s.index(suffix)
 
     return s[preIdx + len(prefix):sufIdx]
+
 
 def _doAtomicFileCreation(filePath):
     """Tries to atomically create the requested file."""
@@ -25,13 +28,14 @@ def _doAtomicFileCreation(filePath):
 
 def _findNextFile(folder, prefix, suffix, fnameGen, base, maxattempts, loop):
     if loop >= maxattempts:
-        raise OSError("Unable to create file at " + folder + " after " + str(maxattempts) + " attempts.")
+        raise OSError("Unable to create file at " + folder +
+                      " after " + str(maxattempts) + " attempts.")
 
     if prefix is None and fnameGen is None:
-        raise RuntimeError("Need at least one of prefix or fnameGen to proceed.")
+        raise RuntimeError("Need at least one of prefix/suffix or fnameGen.")
 
     if (prefix is not None or suffix is not None) and fnameGen is not None:
-        raise RuntimeError("Cannot provide prefix/suffix as well as an fnameGen.")
+        raise RuntimeError("Cannot provide both prefix/suffix and a fnameGen.")
 
     nextFileIdx = base
 
@@ -43,15 +47,17 @@ def _findNextFile(folder, prefix, suffix, fnameGen, base, maxattempts, loop):
         prefix = prefix if prefix is not None else ''
         suffix = suffix if suffix is not None else ''
 
-        globPattern = lambda x : _os.path.join(folder, prefix + x + suffix)
-        allFiles = _natsort.natsorted(_glob.glob(globPattern('*')), alg=_natsort.ns.INT)
+        globPattern = _os.path.join(folder, prefix + '*' + suffix)
+        allFiles = _natsort.natsorted(_glob.glob(globPattern),
+                                      alg=_natsort.ns.INT)
         # Not using complete path here, since Windows paths contain
         # back-slashes, which will be interpreted as escaped special regex.
-        selectFilesRegEx = _re.compile(prefix + '[0-9]+' + suffix + '$')
-        numberedFiles = [ f for f in allFiles if _re.search(selectFilesRegEx, f) ]
+        numFilesRegEx = _re.compile(prefix + '[0-9]+' + suffix + '$')
+        numberedFiles = [f for f in allFiles if _re.search(numFilesRegEx, f)]
 
         if len(numberedFiles) > 0:
-            nextFileIdx = int(_getStrBetween(prefix, numberedFiles[-1], suffix)) + 1
+            lastFileNum = int(_strBetween(prefix, numberedFiles[-1], suffix))
+            nextFileIdx = lastFileNum + 1
 
     if fnameGen is None:
         retFileName = _os.path.join(folder, prefix + str(nextFileIdx) + suffix)
@@ -61,16 +67,21 @@ def _findNextFile(folder, prefix, suffix, fnameGen, base, maxattempts, loop):
     if _doAtomicFileCreation(retFileName):
         return retFileName
     else:
-        return _findNextFile(folder, prefix, suffix, fnameGen, base, maxattempts, loop + 1)
+        return _findNextFile(folder, prefix, suffix, fnameGen,
+                             base, maxattempts, loop + 1)
 
 
+###############################################################################
+# Public interface
+###############################################################################
 
-###########################################################################################
-## Public interface
-###########################################################################################
 
-
-def findNextFile(folder='.', prefix=None, suffix=None, fnameGen=None, base=0, maxattempts=10):
+def findNextFile(folder='.',
+                 prefix=None,
+                 suffix=None,
+                 fnameGen=None,
+                 base=0,
+                 maxattempts=10):
     """Finds the next available file-name in a sequence.
 
     This function will create a file of zero size and will return a path to it
@@ -93,21 +104,25 @@ def findNextFile(folder='.', prefix=None, suffix=None, fnameGen=None, base=0, ma
 
 
     Args:
-        folder      - string which has path to the folder where the file should be created (default: '.')
+        folder      - string which has path to the folder where the file should
+                      be created (default: '.')
         prefix      - prefix of the file to be generated (default: '')
         suffix      - suffix of the file to be generated (default: '')
-        fnameGen    - function which generates the filenames given a number as input (default: None)
+        fnameGen    - function which generates the filenames given a number as
+                      input (default: None)
         base        - the first index to count (default: 0)
-        maxattempts - number of attempts to create the file before failing with OSError (default: 10)
+        maxattempts - number of attempts to create the file before failing with
+                      OSError (default: 10)
 
     Returns:
-        Path of the file which follows the provided pattern and can be written to.
+        Path of the file which follows the provided pattern and can be opened
+        for writing.
 
     Raises:
         RuntimeError - If an incorrect combination of arguments is provided.
-        OSError      - If is unable to create a file (wrong path, drive full, etc.).
+        OSError      - If is unable to create a file (wrong path, drive full,
+                       illegal character in filename, etc.).
     """
     expFolder = _os.path.expanduser(_os.path.expandvars(folder))
-    return _findNextFile(expFolder, prefix, suffix, fnameGen, base, maxattempts, 0)
-
-
+    return _findNextFile(expFolder, prefix, suffix, fnameGen,
+                         base, maxattempts, 0)
